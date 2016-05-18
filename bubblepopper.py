049b667f7c82787966f170ea7438e8905ea14c80
b-franka <b.franku@gmail.com>
2016-05-18 16:12:04 +0200

    not good

--- bubblepopper.py
+++ bubblepopper.py
@@ -5,12 +5,11 @@ import time
 import random
 from curses import KEY_ENTER, KEY_RIGHT
 
-def main(scr):
+def main(screen):
     curses.noecho()
     curses.curs_set(0)
 
     screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
-    screen.border(0)
     screen.keypad(1)
     screen.nodelay(1)
     key = KEY_RIGHT
@@ -21,12 +20,14 @@ def main(scr):
         if keystroke in [KEY_ENTER, 27]:
             key = keystroke
 
-        for z in range (curses.LINES - 1):
+        for z in range (curses.LINES - 2):
             screen.clear()
+            screen.border(0)
             screen.addstr(0, curses.COLS // 2 - 9, "Castle Rock Popper")
             screen.addstr(z + 1, curses.COLS // 2, rock)
             screen.refresh()
             time.sleep(0.05)
 
+            if key == 27: break
 
 curses.wrapper(main)
