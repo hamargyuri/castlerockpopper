d8088479dde2da5906aad1c00b5acdc58f50c7ac
hamargyuri <gyurinak@gmail.com>
2016-05-18 16:10:23 +0200

    for loop removed

--- bubblepopper.py
+++ bubblepopper.py
@@ -10,23 +10,29 @@ def main(scr):
     curses.curs_set(0)
 
     screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
-    screen.border(0)
     screen.keypad(1)
     screen.nodelay(1)
     key = KEY_RIGHT
+    z = 1
+    rock = str(random.randint(0, 9))
 
     while key != 27:
-        rock = str(random.randint(0, 9))
         keystroke = screen.getch()
         if keystroke in [KEY_ENTER, 27]:
             key = keystroke
+        if z == int(curses.LINES - 1):
+            rock = str(random.randint(0, 9))
+            z = 1
 
-        for z in range (curses.LINES - 1):
-            screen.clear()
-            screen.addstr(0, curses.COLS // 2 - 9, "Castle Rock Popper")
-            screen.addstr(z + 1, curses.COLS // 2, rock)
-            screen.refresh()
-            time.sleep(0.05)
+        screen.clear()
+        screen.border(0)
+        screen.addstr(0, curses.COLS // 2 - 9, "Castle Rock Popper")
+        screen.addstr(z, curses.COLS // 2, rock)
+        screen.refresh()
+        time.sleep(0.05)
+        z = z + 1
+
+        if key == 27: break
 
 
 curses.wrapper(main)
