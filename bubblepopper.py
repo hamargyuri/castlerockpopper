dc2f3e75aeb28c7579ae2a14ab95ae0826c6aa6d
hamargyuri <gyurinak@gmail.com>
2016-05-19 13:47:32 +0200

    rock displays equation, result is stored in variable

--- bubblepopper.py
+++ bubblepopper.py
@@ -3,7 +3,8 @@
 import curses
 import time
 import random
-from curses import KEY_ENTER
+result = ""
+
 
 def main(screen):
     curses.noecho()
@@ -13,7 +14,23 @@ def main(screen):
     screen.keypad(1)
     screen.nodelay(1)
 
-    rock = str(random.randint(0, 9))
+    def generate_rock():
+        global result
+        num_1 = int(random.randint(0, 9))
+        num_2 = int(random.randint(1, 9))
+        operators = ["+", "-", "*", "/"]
+        rock_operator = str(random.choice(operators))
+        if rock_operator == "+":
+            result = num_1 + num_2
+        if rock_operator == "-":
+            result = num_1 - num_2
+        if rock_operator == "*":
+            result = num_1 * num_2
+        if rock_operator == "/":
+            result = num_1 // num_2
+        return (str(num_1) + str(rock_operator) + str(num_2))
+
+    rock = str(generate_rock())
     key = 0
     rock_y = 1
 
@@ -22,9 +39,10 @@ def main(screen):
         screen.clear()
         screen.border(0)
         screen.addstr(0, curses.COLS // 2 - 9, "Castle Rock Popper")
-        screen.addstr(rock_y, curses.COLS // 2, rock)
+        screen.addstr(0, 0, str(result))    #just testing if result is working
+        screen.addstr(rock_y, curses.COLS // 2 - len(rock) // 2, rock)
         screen.refresh()
-        time.sleep(0.05)
+        time.sleep(0.1)
         rock_y = rock_y + 1
         keystroke = screen.getch()
 
@@ -32,11 +50,11 @@ def main(screen):
             key = keystroke
 
         if rock_y == int(curses.LINES - 1):     #new rock if reached bottom
-            rock = str(random.randint(0, 9))
+            rock = str(generate_rock())
             rock_y = 1
 
         if key == 32:               #space is pressed (how to change to enter?)
-            rock = str(random.randint(0, 9))
+            rock = str(generate_rock())
             rock_y = 1
 
         if key == 27: break         #quit if esc is pressed
