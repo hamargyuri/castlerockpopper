8e76c87d0ecec1f6f2d638d89438f916ea7dfcaf
b-franka <b.franku@gmail.com>
2016-05-19 11:04:44 +0200

    text

--- bubblepopper.py
+++ bubblepopper.py
@@ -4,19 +4,22 @@ import curses
 import time
 import random
 from curses import KEY_ENTER
+from curses import textpad
 
 def main(screen):
-    curses.noecho()
-    curses.curs_set(0)
+    curses.noecho()             # read keystrokes instantly, without waiting for enter to ne pressed
+    curses.curs_set(0)          # set cursor visibility to invisible
+    curses.cbreak()             # enable keypad mode
 
     screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
-    screen.keypad(1)
+    screen.keypad(1)            # enable processing of functional keys by curses
     screen.nodelay(1)
     key = 0
     rock_y = 1
     rock = str(random.randint(0, 9))
 
     while key != 27:
+        new_text()
         keystroke = screen.getch()
         if keystroke in [KEY_ENTER, 27]:
             key = keystroke
@@ -27,7 +30,7 @@ def main(screen):
         screen.clear()
         screen.border(0)
         screen.addstr(0, curses.COLS // 2 - 9, "Castle Rock Popper")
-        screen.addstr(z, curses.COLS // 2, rock)
+        screen.addstr(rock_y, curses.COLS // 2, rock)
         screen.refresh()
         time.sleep(0.05)
         rock_y = rock_y + 1
